f1afdbd963a2e5c4c5521fe3704a17124d61e1c3
Waclaw Gajda <waclaw.gajda@gmail.com>
2017-11-23 18:22:30 +0100

    zadania.sql 23.11

--- PostgreSQL/zadania.sql
+++ PostgreSQL/zadania.sql
@@ -1,6 +1,6 @@
 use computer_store
-select * from products
-select * from manufacturers
+select * from products;
+select * from manufacturers;
 --zad 1
 select name from products;
 --zad 2
@@ -18,5 +18,28 @@ select avg(price) as srednia_cena from products where manufacturer_id = 2;
 --zad 8
 select count(*) as ilosc_prod180 from products where price >= 180;
 --zad 9
-select name, price from products where price >= 180 order by price desc;
-select name, price from products where price >= 180 order by name;
+select name, price from products where price >= 180 order by name, price desc;
+--zad 10
+select products.*, manufacturers.name as Producent from products join manufacturers ON manufacturers.id = products.manufacturer_id;
+--zad 11
+select products.name as produkt, products.price as cena, manufacturers.* from products join manufacturers ON manufacturers.id = products.manufacturer_id;
+--zad 12
+select manufacturers.id, avg(price) as srednia_cena from products join manufacturers ON manufacturers.id = products.manufacturer_id group by manufacturers.id;
+--zad 13
+select manufacturers.name, avg(price) as srednia_cena from products join manufacturers ON manufacturers.id = products.manufacturer_id group by manufacturers.name;
+--zad 14
+--nie wiem
+--zad 15 nie ud�ao mi si� po�aczy� min() z nazw� produktu
+select name, price from products order by price limit 1;
+--zad 16
+select name from manufacturers
+--zad 17
+select
+--zad 18
+insert into products (name, price, manufacturer_id) values ('Loudspeakers', 70, 2);
+--zad 19
+update products set name = 'Laser Printer' where id = 8;
+--zad 20
+update products set price = price-price*0.1;
+--zad 21
+update products set price = price-price*0.1 where price > 120;
